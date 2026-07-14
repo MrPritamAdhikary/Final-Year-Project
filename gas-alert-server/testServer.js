d4c28925cadfab29324359700c71d9153b866d57
MrPritamAdhikary <mr.pritamadhikary@gmail.com>
2026-07-15 01:23:05 +0530

    Create testServer.js

---
+++ gas-alert-server/testServer.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const { initializeApp, cert } = require("firebase-admin/app");
+const { getMessaging } = require("firebase-admin/messaging");
+
+const serviceAccount = require("./serviceAccountKey.json");
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const token = process.env.FCM_DEVICE_TOKEN;
+
+const message = {
+  notification: {
+    title: "GAS LEAK TEST",
+    body: "Direct Firebase notification test successful."
+  },
+  token: token
+};
+
+getMessaging()
+  .send(message)
+  .then((response) => {
+    console.log("DIRECT FCM SENT SUCCESSFULLY");
+    console.log(response);
+  })
+  .catch((error) => {
+    console.log("FCM ERROR CODE:", error.code);
+    console.log("FCM ERROR MESSAGE:", error.message);
+  });
